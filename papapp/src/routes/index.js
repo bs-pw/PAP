909091ffd07532055a0189db65326a27f4afff73
Patryk Skręta <01187282@pw.edu.pl>
2024-12-09 08:05:24 +0100

    Protect auth routes

--- papapp/src/routes/index.js
+++ papapp/src/routes/index.js
@@ -3,6 +3,38 @@ import AppLayout from "../components/layout/AppLayout";
 import LoginPage from "../pages/LoginPage"
 import DashboardPage from "../pages/DashboardPage"
 import MainLayout from "../components/layout/MainLayout";
+import { Navigate, Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+//import useAuthStatus from './useAuthStatus';
+
+const ProtectedRoute = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const checkAuthStatus = async () => {
+            try {
+                const response = await fetch('/api/auth/status', {
+                    credentials: 'include',
+                });
+                const data = await response.json();
+                setIsAuthenticated(data.loggedIn);
+            } catch (error) {
+                console.error('Błąd sprawdzania statusu logowania', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        checkAuthStatus();
+    }, []);
+
+    if (loading) {
+        return <div>Ładowanie...</div>;
+    }
+
+    return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
 
 export const router = createBrowserRouter([
     {
@@ -11,18 +43,23 @@ export const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <LoginPage />
+                element: <LoginPage />,
             },
             {
                 path: "dashboard",
-                element: <MainLayout />,
+                element: <ProtectedRoute />, // Chroniona trasa
                 children: [
                     {
-                        index: true,
-                        element: <DashboardPage />
-                    }
-                ]
-            }
-        ]
-    }
-])
+                        element: <MainLayout />,
+                        children: [
+                            {
+                                index: true,
+                                element: <DashboardPage />,
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+]);
