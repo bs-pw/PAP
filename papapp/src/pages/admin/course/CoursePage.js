2be723ad87a57dca5b2ae802f3e47d2dd9b0762d
Patryk Skręta <01187282@pw.edu.pl>
2025-01-12 01:41:09 +0100

    Refactor CoursePage to use List component and remove CoursesList; implement course fetching logic directly in CoursePage

--- papapp/src/pages/admin/course/CoursePage.js
+++ papapp/src/pages/admin/course/CoursePage.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import CoursesList from '../../../components/common/CoursesList'
+import CoursesList from '../../../components/common/List'
+import { useClient } from '../../../components/ClientContext'
+import { useState, useEffect } from 'react'
 
 const CoursePage = () => {
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+    const client = useClient();
+
+    const getCourses = async () => {
+        try {
+            const data = await client.getCourses();
+            console.log(data);
+            setUsers(data);
+        } catch (error) {
+            setError('Błąd podczas ładowania danych: ' + error.message);
+        }
+    }
+
+    useEffect(() => {
+        getCourses();
+    }, []);
+
+
     return (
         <>
             <Link to="/admin/courses/create" className='nav-link text-primary' style={{ fontSize: "1.2em" }}><i class="bi bi-plus-lg"></i> Nowy</Link >
-            <CoursesList adminButtons={true} />
+            <CoursesList listName='Lista kursów' adminButtons={true} columnNames={['ID', 'Nazwa', 'Opis', 'Cena', 'Czas trwania']} data={users} error={error} />
         </>
     )
 }
