f5d6f970b743bb39e0052e39dfbec076c26fd88e
Maciej Borkowski <01187516@pw.edu.pl>
2025-01-15 19:24:16 +0100

    getCourseInSemesterBy

--- papapi/src/main/java/pap/z27/papapi/repo/CourseInSemesterRepo.java
+++ papapi/src/main/java/pap/z27/papapi/repo/CourseInSemesterRepo.java
@@ -32,8 +32,9 @@ public class CourseInSemesterRepo {
                 .query(CourseInSemester.class)
                 .list();
     }
-    public List<CourseInSemester> findAllCoursesInSemesterByLecturer(String semester, Integer userId) {
-        return jdbcClient.sql("SELECT cis.* from COURSES_IN_SEMESTER cis join LECTURERS c on cis.semester =c.semester and cis.course_code=c.course_code where c.user_id=? and cis.semester=?")
+
+    public List<CourseInSemester> findAllCoursesInSemesterByLecturerAndCoordinator(String semester, Integer userId) {
+        return jdbcClient.sql("SELECT cis.* from COURSES_IN_SEMESTER cis join LECTURERS c on cis.semester =c.semester and cis.course_code=c.course_code where c.user_id=? and cis.semester=? UNION SELECT cis.* from COURSES_IN_SEMESTER cis join COORDINATORS c on cis.semester =c.semester and cis.course_code=c.course_code where c.user_id=? and cis.semester=?")
                 .param(userId)
                 .param(semester)
                 .query(CourseInSemester.class)
