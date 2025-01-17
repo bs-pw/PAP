c857553bac3278b52c1c8c1c5de1c9c4fc9f56e4
Patryk Skręta <01187282@pw.edu.pl>
2025-01-17 02:33:47 +0100

    `Refactor lecturer and student menu items, add student semester page and update routes`

---
+++ papapp/src/pages/student/StudentSemesterPage.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { useClient } from '../../components/ClientContext';
+import { Link, useNavigate } from 'react-router-dom';
+import List from '../../components/common/List';
+
+const StudentSemesterPage = () => {
+    const [semesters, setSemesters] = useState([]);
+    const [error, setError] = useState('');
+    const client = useClient();
+    const navigate = useNavigate();
+
+    const getSemesters = async () => {
+        try {
+            const data = await client.getSemesterByStudent(client.userId);
+            setSemesters(data);
+        } catch (error) {
+            setError('Błąd podczas ładowania danych: ' + error.message);
+        }
+    };
+
+    const handleDeleteSemester = async (e) => {
+        e.preventDefault();
+        // console.log("ti")
+        try {
+            if (await client.deleteSemester(e.target.value)) {
+                await getSemesters();
+            }
+        } catch (error) {
+            setError('Błąd podczas usuwania semestru!');
+        }
+    }
+
+    const handleEditSemester = (e) => {
+        e.preventDefault();
+        navigate(`/admin/semesters/edit/` + e.target.value);
+    }
+
+    const handleVievCoursesInSemester = (e) => {
+        e.preventDefault();
+        navigate(`/admin/semesters/${e.target.value}/courses`);
+    }
+
+    useEffect(() => {
+        getSemesters();
+    }, []);
+
+    console.log(semesters);
+
+    return (
+        <>
+            {client.userTypeId == 0 && <Link to="/admin/semesters/create" className='nav-link text-primary' style={{ fontSize: "1.2em" }}><i className="bi bi-plus-lg"></i> Nowy</Link >}
+            <List listName='Lista semestrów' columnNames={['Kod', 'Data początku', 'Data końca']} data={semesters} error={error} adminButtons={client.userTypeId == 0 && true} userButtons={true} handleDelete={handleDeleteSemester} handleEdit={handleEditSemester} handleViev={handleVievCoursesInSemester} id="semester_code" />
+        </>
+    )
+}
+
+export default StudentSemesterPage
