ca7d14c81a2dbe2c705b2a113a1b013271d6a43f
Patryk Skręta <01187282@pw.edu.pl>
2025-01-17 03:44:56 +0100

    `feat: updated StudentFinalGradesInSem.js with React hooks and API call`

--- papapp/src/pages/student/StudentFinalGradesInSem.js
+++ papapp/src/pages/student/StudentFinalGradesInSem.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useClient } from '../../components/ClientContext';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import List from '../../components/common/List';
 
 const StudentFinalGradesInSem = () => {
+    const [finalGrades, setFinalGrades] = useState([]);
+    const [error, setError] = useState('');
+    const client = useClient();
+    const navigate = useNavigate();
+    const { semesterId } = useParams();
 
+    const getStudentFinalGradesInSem = async () => {
+        try {
+            const data = await client.getStudentsFinalGrades(semesterId, client.userId);
+            const filtered = data.map(({ course_code, grade }) => ({ course_code, grade }))
+            setFinalGrades(filtered);
+        } catch (error) {
+            setError('Błąd podczas ładowania danych: ' + error.message);
+        }
+    }
+
+    useEffect(() => {
+        getStudentFinalGradesInSem();
+    }, []);
 
     return (
-        <div>StudentFinalGradesInSem</div>
+        <List listName={`Oceny końcowe w ${semesterId}`} columnNames={['Przedmiot', 'Ocena']} data={finalGrades} error={error} />
     )
 }
 
