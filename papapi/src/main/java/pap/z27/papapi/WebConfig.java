3d8778c0386dad833efc1924d79de25f9557ce75
Patryk Skręta <01187282@pw.edu.pl>
2025-01-17 03:41:00 +0100

    add serving static content in java app

---
+++ papapi/src/main/java/pap/z27/papapi/WebConfig.java
@@ -0,0 +1,14 @@
+package pap.z27.papapi;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/{spring:[^\\.]*}")
+//                .setViewName("forward:/index.html");
+//    }
+}
