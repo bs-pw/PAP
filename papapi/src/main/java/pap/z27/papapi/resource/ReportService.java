8aee4785974df86d194bf9213fd36103392386c2
mdomagal <mdomagal@stud.elka.pw.edu.pl>
2025-01-22 02:30:03 +0100

    UNFINISHED: building out PDF report module

--- papapi/src/main/java/pap/z27/papapi/resource/ReportService.java
+++ papapi/src/main/java/pap/z27/papapi/resource/ReportService.java
@@ -1,10 +1,9 @@
 package pap.z27.papapi.resource;
 
 import com.lowagie.text.*;
-import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.*;
+import com.lowagie.text.pdf.PdfTable;
 import com.lowagie.text.Chunk;
-import com.lowagie.text.pdf.LayoutProcessor;
-import com.lowagie.text.pdf.PdfWriter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 import pap.z27.papapi.domain.subclasses.NameGrade;
@@ -32,6 +31,8 @@ public class ReportService {
 
 
         PdfWriter.getInstance(document, response.getOutputStream());
+        float width = document.getPageSize().getWidth();
+        float height = document.getPageSize().getHeight();
 
         document.open();
         Font titleFont = FontFactory.getFont("notoSansBold", BaseFont.IDENTITY_H, true, 20);
@@ -41,7 +42,7 @@ public class ReportService {
         Paragraph title = new Paragraph("PROTOKÓŁ KOŃCOWY", titleFont);
         title.setAlignment(Element.ALIGN_CENTER);
         document.add(title);
-        document.add(Chunk.NEWLINE);
+        document.add(new Paragraph(Chunk.NEWLINE));
 
         Paragraph daneHeader = new Paragraph(new Chunk("I: Dane przedmiotu", headerFont));
         document.add(daneHeader);
@@ -52,6 +53,33 @@ public class ReportService {
         daneText.add("Koordynator(rzy): " + String.join(", ", coordinators) + "\n");
         daneText.add("Prowadzący: " + String.join(", ", lecturers) + "\n");
         document.add(daneText);
+        document.add(new Paragraph(Chunk.NEWLINE));
+
+        Paragraph uczestnicyHeader = new Paragraph(new Chunk("II: Uczestnicy\n", headerFont));
+        document.add(uczestnicyHeader);
+
+        float[] columnDefinitionSize = {33.33F, 33.33F, 33.33F};
+        PdfPTable table = new PdfPTable(columnDefinitionSize);
+        table.getDefaultCell().setBorder(0);
+        table.setHorizontalAlignment(Element.ALIGN_LEFT);
+        table.setTotalWidth(width - 20);
+        table.setLockedWidth(true);
+
+        PdfPCell cell = new PdfPCell();
+        cell.setColspan(columnDefinitionSize.length);
+        table.addCell(cell);
+        table.addCell(new Phrase("Louis Pasteur", textFont));
+        table.addCell(new Phrase("Albert Einstein", textFont));
+        table.addCell(new Phrase("Isaac Newton", textFont));
+        table.addCell(new Phrase("8, Rabic street", textFont));
+        table.addCell(new Phrase("2 Photons Avenue", textFont));
+        table.addCell(new Phrase("32 Gravitation Court", textFont));
+        table.addCell(new Phrase("39100 Dole France", textFont));
+        table.addCell(new Phrase("12345 Ulm Germany", textFont));
+        table.addCell(new Phrase("45789 Cambridge  England", textFont));
+
+        document.add(table);
+
 
 //        Paragraph signature = new Paragraph("........", textFont);
 //        signature.setAlignment(Element.ALIGN_RIGHT);
