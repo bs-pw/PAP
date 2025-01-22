e31aa09c49d1e4485438b679f42038603c9fe01a
Patryk Skręta <01187282@pw.edu.pl>
2025-01-22 21:21:03 +0100

    `Added password confirmation fields and validation in MyDetails and UserForm components`

--- papapp/src/pages/MyDetails.js
+++ papapp/src/pages/MyDetails.js
@@ -6,6 +6,7 @@ const MyDetails = () => {
 
     const [mail, setMail] = useState();
     const [password, setPassword] = useState();
+    const [password2, setPassword2] = useState();
     const [message, setMessage] = useState(" ");
     const client = useClient();
 
@@ -30,6 +31,7 @@ const MyDetails = () => {
 
     const changePassword = async (e) => {
         e.preventDefault()
+        if (password != password2) return;
         try {
             await client.updateUser(client.userId, { password: password })
             setMessage("Zmieniono hasło!")
@@ -38,22 +40,30 @@ const MyDetails = () => {
         }
     }
 
+    const handleChangePassword2 = (e) => {
+        setPassword2(e.target.value);
+        password != password2 ? setMessage('Hasła nie są identyczne') : setMessage(' ');
+    }
+
 
     useEffect(() => {
         client.checkAuthStatus();
         setMail(client.mail)
-        setPassword(client.password)
+        // setPassword(client.password)
     }, []);
 
     const mailForm = [{ name: 'mail', label: 'E-mail', type: 'email', value: mail, required: true, onChange: handleChangeMail }];
-    const passwordForm = [{ name: 'password', label: 'Hasło', type: 'password', value: password, required: true, onChange: handleChangePassword }];
+    const passwordForm = [
+        { name: 'password', label: 'Hasło', type: 'password', value: password, required: true, onChange: handleChangePassword },
+        { name: 'password2', label: 'Powtórz hasło', type: 'password', value: password2, required: true, onChange: handleChangePassword2 },
+    ];
 
     return (
         <div>
             <h1>Moje dane</h1>
             <p>Imię: {client.name}</p>
             <p>Nazwisko: {client.surname}</p>
-            {message && <p style={{ color: 'green' }}>{message}</p>}
+            {message && <p>{message}</p>}
             <Form inputData={mailForm} buttonName="Zmień" formName={"Zmień e-mail"} onSubmit={changeMail} />
             <Form inputData={passwordForm} buttonName="Zmień" formName={"Zmień hasło"} onSubmit={changePassword} />
         </div>
